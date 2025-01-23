aa0bfce59d94e8a274df27498a2a3da54b8a73e2
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-01-23 23:05:14 +0530

    multiplication

---
+++ loop/multiplication_table.java
@@ -0,0 +1,10 @@
+package loop;
+
+public class multiplication_table {
+    public static void main(String[] args) {
+        int num = 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + "x " + i + " = " + (num * i));
+        }
+    }
+}
