02ed7c727a48def954d35c7739a060a3323c3493
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-01-24 20:08:23 +0530

    even no.

---
+++ loop/evenno.java
@@ -0,0 +1,12 @@
+package loop;
+
+public class evenno {
+    public static void main(String[] args) {
+        int n = 20;
+        for (int i = 2; i < n; i+=2) {
+            System.out.println(i);
+            
+        }
+
+    }
+}
