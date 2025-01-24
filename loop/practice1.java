1aa4bb8eab7d12d79ca73131e3f917de2b6dcc5f
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-01-24 20:11:42 +0530

    practice

---
+++ loop/practice1.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class practice1 {
+    public static void main(String[] args) {
+        for(;;){
+            System.out.println("Apna lelo");
+        }
+    }
+}
+
+
+// for(;;):
+
+// A for loop without initialization, condition, or increment behaves as an infinite loop.
+// It continuously executes the block of code inside it.
+// System.out.println("Apna lelo");:
+
+// This line will be printed endlessly because the loop never terminates.
