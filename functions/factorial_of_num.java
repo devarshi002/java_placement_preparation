df952c586bd9bcaa5d2dde45807de6af9e40b673
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-01-27 21:24:50 +0530

    function to write factorial

---
+++ functions/factorial_of_num.java
@@ -0,0 +1,25 @@
+package functions;
+
+import java.util.Scanner;
+
+public class factorial_of_num {
+    
+    public static void Factorial(int n){
+        int fact = 1;
+        System.out.println("Factorial of number "+ n + " is : ");
+        for (int i = n; i>=1; i--) {
+            fact = fact * i;
+        }
+        
+        System.out.println(fact);
+        
+    }
+    
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter number for factorial: ");
+            int n = sc.nextInt();
+            Factorial(n);
+        }
+    }
+}
