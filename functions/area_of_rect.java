361e164539c95e361f652b401ad4e42b43dfea33
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-01-27 20:18:01 +0530

    functions to write area of rect

---
+++ functions/area_of_rect.java
@@ -0,0 +1,16 @@
+package functions;
+
+public class area_of_rect {
+    
+    public static void CalculateArea(int length, int width){
+        int area = length * width;
+        System.out.println(area);
+    }
+    
+    
+    
+    
+    public static void main(String[] args) {
+        CalculateArea(5, 10);
+    }
+}
