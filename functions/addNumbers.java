728905dddc33906a5847a4d66a7fceb8ae6fbf7e
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-01-27 20:10:17 +0530

    functions to add two numbers

---
+++ functions/addNumbers.java
@@ -0,0 +1,15 @@
+package functions;
+
+public class addNumbers {
+    
+    public static void Add_numbers(int a, int b){
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        Add_numbers(5, 03);
+    }
+}
