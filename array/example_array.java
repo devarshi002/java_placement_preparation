abc15d0778fe5ad7ac229d97be09dbfe8efa694f
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-01-28 22:18:25 +0530

    array introduction

---
+++ array/example_array.java
@@ -0,0 +1,14 @@
+package array;
+
+public class example_array {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 97;
+        marks[1] = 80;
+        marks[2] = 92;
+
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
