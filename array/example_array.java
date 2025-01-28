563606529898b2b9c86c8802b483b4b2c5e816c4
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-01-28 22:20:16 +0530

    array introduction

--- array/example_array.java
+++ array/example_array.java
@@ -7,8 +7,12 @@ public class example_array {
         marks[1] = 80;
         marks[2] = 92;
 
-        System.out.println(marks[0]);
-        System.out.println(marks[1]);
-        System.out.println(marks[2]);
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);
+        }
     }
 }
