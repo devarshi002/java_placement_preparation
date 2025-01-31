1cbf097dddb172c64e144d64115f679b7f84899c
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-01-31 22:56:46 +0530

    string in java

---
+++ String/Example.java
@@ -0,0 +1,8 @@
+package String;
+
+public class Example {
+    public static void main(String[] args) {
+        String str = new String("GeeksforGeeks");
+        System.out.println(str);
+    }
+}
