80ccb8b8bb430e4c954563c34c0b13f344ee144f
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-02-02 22:18:52 +0530

    string in java

---
+++ String/Substring.java
@@ -0,0 +1,9 @@
+package String;
+
+public class Substring {
+    public static void main(String[] args) {
+        String sent = "My name is java";
+        String name = sent.substring(11 );
+        System.out.println(name);
+    }
+}
