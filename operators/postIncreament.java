222de0570c5b8c3e350a3a8c5c24e794a1a592ec
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-02-04 21:31:43 +0530

    operator

---
+++ operators/postIncreament.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class postIncreament {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        b = a++;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
