4e65d52a45abd67595fc76930c74b475169f941b
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-02-04 22:37:22 +0530

    search element leetcode 35

---
+++ array/SearchInsert.java
@@ -0,0 +1,30 @@
+package array;
+
+public class SearchInsert {
+    
+    public int searchInsert(int[] nums, int target){
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }else if (nums[mid] < target) {
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+    
+    
+    public static void main(String[] args) {
+        SearchInsert se = new SearchInsert();
+        int[] nums = {1,3,5,6};
+        int target = 5;
+
+        int result = se.searchInsert(nums, target);
+        System.out.println("Index: " + result);
+    }
+}
