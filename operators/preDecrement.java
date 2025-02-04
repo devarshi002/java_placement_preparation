92d25b91e840e288bc30017db01383f9f48fd156
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-02-04 21:34:48 +0530

    operator

---
+++ operators/preDecrement.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class preDecrement {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        b = --a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
