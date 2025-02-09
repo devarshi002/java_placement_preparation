f02e67ac748421a55115d87d4cab21e82817f392
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-02-09 12:56:02 +0530

    multiple inheritance in java

---
+++ oops/MultipleInheritance.java
@@ -0,0 +1,26 @@
+package oops;
+
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        C obj = new C();
+        obj.methodA();
+        obj.methodB();
+    }
+}
+
+interface A {
+    void methodA();
+}
+
+interface B {
+    void methodB();
+}
+
+class C implements A, B {
+    public void methodA() {
+        System.out.println("Method A");
+    }
+    public void methodB() {
+        System.out.println("Method B");
+    }
+}
