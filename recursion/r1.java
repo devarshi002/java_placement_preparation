279c55bf904c371184985559c00373f508c0a7c9
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-02-13 22:11:35 +0530

    recursion in java

---
+++ recursion/r1.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class r1 {
+    public static void main(String[] args) {
+        int n = 5;
+        printNumb(n);
+    }
+
+    public static void printNumb(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        printNumb(n-1);
+    }
+}
